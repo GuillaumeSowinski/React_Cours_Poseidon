dee1451cc44574a90df512c8a0659c06a158fb17
Guillaume <guillaume@sowinski.fr>
2025-09-22 17:14:31 +0200

    full fixed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <main>
         <Container >
           <Row >
-            <Col s>
+            <Col md={4} >
               <Dish
                 price={3}
                 image={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"}
@@ -21,7 +21,7 @@ function App() {
                 name={"Tacos à l'unité"}
               />
             </Col>
-            <Col>
+            <Col md={4} >
               <Dish
                 price={12}
                 image={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"}
@@ -29,7 +29,7 @@ function App() {
                 name="Enchiladas"
               />
             </Col>
-            <Col>
+            <Col md={4} >
               <Dish
                 price={15}
                 image={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"}
