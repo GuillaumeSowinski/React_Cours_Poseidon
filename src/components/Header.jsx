22de6c831b1db9612a8f2d190baf4fb9b258ffb1
Guillaume <guillaume@sowinski.fr>
2025-09-22 11:40:23 +0200

    add menu + delete vite img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 export function Header() {
     return (
         <header>
-            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" ></img>
+            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" />
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
