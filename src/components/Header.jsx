feabaae074f8dbd737dc893e6ef1342da1af1a9f
Guillaume <guillaume@sowinski.fr>
2025-09-22 11:45:50 +0200

    add alt on img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header>
-            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" />
+            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Musicien avec un chapeau mexicain qui fait de la guitare" />
             <nav>
                 <ul>
                     <li><a href="/">Accueil</a></li>
