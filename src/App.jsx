a799bfc80a625130b91236f84a8cdb7ee80989c0
Guillaume <guillaume@sowinski.fr>
2025-09-23 11:34:23 +0200

    condition isNew dish.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
                 image={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"}
                 altImage={"Tacos garnis succulent avec de la salade autour et un pot de sauce"}
                 name={"Tacos à l'unité"}
+                isNew={true}
               />
             </Col>
             <Col md={4} >
@@ -28,6 +29,7 @@ function App() {
                 image={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"}
                 altImage={"Image d'Enchiladas avec un demi citron vert sur le côté"}
                 name="Enchiladas"
+                isNew={false}
               />
             </Col>
             <Col md={4} >
@@ -36,6 +38,7 @@ function App() {
                 image={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"}
                 altImage={"Une limace de chocolat accompagnée de carottes rapées et des doritos sur de la sauce"}
                 name="Mole poblano"
+                isNew={true}
               />
             </Col>
           </Row>
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles/dish.scss';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
 
-export function Dish({ price, image, altImage, name }) {
+export function Dish({ price, image, altImage, name, isNew }) {
     return (
         <Card>
+            {isNew ? <Badge bg="primary">New</Badge> : null}
             <Card.Img src={image} alt={altImage} />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
