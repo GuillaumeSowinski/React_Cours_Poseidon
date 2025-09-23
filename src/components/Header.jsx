de75c0ee037085d54e7c5c131d0760b3347d3ab2
Guillaume <guillaume@sowinski.fr>
2025-09-23 11:10:24 +0200

    asset logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import '../assets/navBar.scss';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import logo from "../assets/logo.png";
 
 export function Header() {
     return (
         <header>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand><img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Mexicain qui joue de la guitare" /></Navbar.Brand>
+                    <Navbar.Brand><img src={logo} alt="Mexicain qui joue de la guitare" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
